6a3cb453698641e0d2fc1ba0da1d78b17d1a5250
Emanuel Arias <Emanuelarias9@gmail.com>
2025-04-08 01:21:55 -0500

    Crear refactorizado a async/await

--- controllers/ArticuloController.js
+++ controllers/ArticuloController.js
@@ -15,46 +15,49 @@ const curso = (req, res) => {
   });
 };
 
-const crear = (req, res) => {
+const crear = async (req, res) => {
   let parametros = req.body;
+
+  // Validación de datos
   try {
     let validarTitulo =
       !validator.isEmpty(parametros.titulo) &&
       validator.isLength(parametros.titulo, { min: 5, max: undefined });
     let validarContenido = !validator.isEmpty(parametros.contenido);
+
     if (!validarTitulo || !validarContenido) {
-      throw new Error("Datos invalidos");
+      throw new Error("Datos inválidos");
     }
   } catch (error) {
     return res.status(400).json({
       status: "Error",
-      mensaje: "Faltan datos",
+      mensaje: "Faltan datos o son inválidos",
+      error: error.message,
     });
   }
 
-  const articulo = new Articulo(parametros);
+  try {
+    const articulo = new Articulo(parametros);
+    const articuloGuardado = await articulo.save();
 
-  articulo
-    .save()
-    .then((articuloGuardado) => {
-      if (!articuloGuardado) {
-        return res.status(400).json({
-          status: "Error",
-          mensaje: "Error al guardar el articulo",
-        });
-      }
-
-      return res.status(200).json({
-        status: "Success",
-        articulo: articuloGuardado,
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
+    if (!articuloGuardado) {
+      return res.status(400).json({
         status: "Error",
-        error,
+        mensaje: "Error al guardar el artículo",
       });
+    }
+
+    return res.status(200).json({
+      status: "Success",
+      articulo: articuloGuardado,
     });
+  } catch (error) {
+    return res.status(500).json({
+      status: "Error",
+      mensaje: "Error interno del servidor",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
